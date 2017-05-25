1e70c21e03abaac09650cf0be2b28f72c18d4b02
Samir Kerimov <samir@code.az>
2017-05-25 20:55:28 +0400

    Accordion

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,6 +1,16 @@
 var frame=document.querySelector(".frame");
-
+var demoDiv=document.querySelector(".demo");
+var count=0;
 function slide(param){
-	
 	frame.style.left=-600*param+"px";
 }
+
+function demo(){
+	count+=1;
+	demoDiv.style.width=count+"px";
+	if(count>=document.body.clientWidth){
+		clearInterval(myInterval);
+	}
+}
+var myInterval=setInterval(demo,1)
+
